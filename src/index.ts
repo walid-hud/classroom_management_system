d929de9f8d897341d126a78a1c91419c02ce82c4
walid-hud <walidthehud@gmail.com>
2025-12-04 17:42:06 +0100

    some progress

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { select, input, number } from "@inquirer/prompts";
 import { log } from "node:console";
 import { readFile, writeFile, access } from "node:fs/promises";
 import { join } from "node:path/win32";
@@ -28,18 +29,63 @@ interface Student {
 	first_name: string;
 	last_name: string;
 	present: boolean;
-	skills: Skill[];
+	skills: Skill[] | undefined;
 }
 class Class_Manger {
 	constructor() {}
-	add_student(student: Student) {}
-	mark_attendance() {}
-	add_skill() {}
-	show_student_record() {}
-	show_valid_students() {}
-	save_data_file() {}
-	load_data_file() {}
-	exit() {}
+	available_skills = [
+		"HTML",
+		"JAVASCRIPT",
+		"CSS",
+		"NODEJS",
+		"REACT",
+		"TYPESCRIPT",
+		"MONGODB",
+		"EXPRESS",
+		"ANGULAR",
+		"JAVA",
+		"SPRINGBOOT",
+		"DOCKER",
+		"KUBERNETES",
+	];
+	async add_student() {
+		const student: Student = {
+			id: generate_id(),
+			first_name: await input({ message: "first name : " }),
+			last_name: await input({ message: "last name : " }),
+			present: false,
+			skills: await this.add_student_skills(),
+		};
+	}
+	async mark_attendance() {}
+	async add_student_skills() {
+		let skills: Skill[] = [];
+		const answer = await select({
+			message: "choose an option ",
+			choices: ["add a skill", "exit"],
+		});
+		if (answer === "exit") {
+			return skills;
+		} else {
+			skills.push({
+				name: await select({
+					message: "select a skill",
+					choices: this.available_skills,
+				}),
+				level: await select({
+					message: "student level ",
+					choices: ["critical", "average", "good", "excellent"],
+				}),
+			});
+			log("skill added !")
+			return skills
+		}
+	}
+	async show_student_record() {}
+	async show_valid_students() {}
+	async save_data_file() {}
+	async load_data_file() {}
+	async exit() {}
 	data: Student[] = [];
 }
 
